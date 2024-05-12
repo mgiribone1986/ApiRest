91392b8c2fe00d5a00d44699bf7b0acc1f2b1f9e
Maxi Giribone <giribonemaximiliano@gmail.com>
2024-05-12 12:41:57 -0300

    Entrega Dom

---
+++ src/app/core/guards/unsaved-changes.guard.ts
@@ -0,0 +1,16 @@
+import { CanDeactivateFn } from '@angular/router';
+
+export const unsavedChangesGuard: CanDeactivateFn<unknown> = (
+  component,
+  currentRoute,
+  currentState,
+  nextState
+) => {
+  // if(confirm('Tiene cambios sin guardar, desea guardarlos?')) {
+  //   ///
+  // } else {
+
+  // }
+
+  return false;
+};
