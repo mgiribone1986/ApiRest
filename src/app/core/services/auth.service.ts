a8debe6f72d4044fdfc2e1a8270e3c8530cad22d
Maxi Giribone <giribonemaximiliano@gmail.com>
2024-05-12 11:24:33 -0300

    Entrega Dom

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IUser } from '../../layouts/dashboard/pages/users/models';
+import { LoginData} from '../../layouts/auth/models';
 
 @Injectable({ providedIn: 'root' })
-export class AuthService {
+export class AuthService{
   private _authUser$ = new BehaviorSubject<IUser | null>(null);
   public authUser$ = this._authUser$.asObservable();
 
-  login(): void {
-    this._authUser$.next({
-      id: 1,
-      createdAt: new Date(),
-      email: 'email@mail.com',
-      firstName: 'Maxi',
-      lastName: 'Giribone',
-      sanciones: 99,
-      role: 'ADMIN',
+  login(data: LoginData): void {
+    if (data.email !=="email@mail.com" || data.password !== '123456'){
+      alert('correo o password incorrectos')
+    }else {
+      this._authUser$.next({
+        id: 1,
+        createdAt: new Date(),
+        email: 'email@mail.com',
+        firstName: 'Maxi',
+        lastName: 'Giribone',
+        sanciones: 99,
+        role: 'ADMIN',
     });
   }
+}
 
   logout(): void {
     this._authUser$.next(null);
   }
 }
+
